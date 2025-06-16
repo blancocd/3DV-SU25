539030ef9a1311d7660b4ba2273c226681882c67
Long Nguyen <long.pollehn@gmail.com>
2025-06-17 00:28:21 +0200

    Added training

---
+++ 3DVision-ex3/pointnet/evaluate_segmentation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH --ntasks=1
+#SBATCH --nodes=1
+#SBATCH --time=1-00:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --partition=day
+#SBATCH --mail-type=FAIL,END
+#SBATCH --mail-user=long.nguyen@student.uni-tuebingen.de
+#SBATCH --mem=64gb
+
+eval "$(~/miniconda3/bin/conda shell.bash hook)"
+if [ -z "$CONDA_INTERPRETER" ]; then
+	export CONDA_INTERPRETER="3DVision-ex3" # Check if CONDA_INTERPRETER is not set, then set it to garage_2
+fi
+source activate "$CONDA_INTERPRETER"
+which python3
+
+python main.py --task "seg" --randomization --load_checkpoint <checkpoint_name> --eval
+python eval_seg.py --i <object_id> --load_checkpoint <checkpoint_name>
